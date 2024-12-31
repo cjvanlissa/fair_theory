9c5837f3a2d0793268f179b87c07b92e435bb90f
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2024-12-31 16:26:06 +0100

    add steps to make theory FAIR

--- add_fair_theory.R
+++ add_fair_theory.R
@@ -1,44 +1,76 @@
 add_fair_theory <- function(path = ".",
                             theory_file = NULL,
-                            add_license = "CC0"
+                            remote_repo = "https",
+                            add_license = c("cc0", "ccby", "gpl", "gpl3", "agpl", "agpl3", "apache", "apl2", "lgpl", "mit", "proprietary", "none")
                             ){
-  # 1. Add theory file
+  # Select first license
+  if(is.null(add_license)) add_license = "none"
+  add_license <- tryCatch(add_license[1], error = function(e){"none"})
+  
+  # 1. Create project folder
+  if(!dir.exists(path)){
+    dir.create(path)
+  }
+  
+  # 2. Initialize Git repo
+  gert::git_init(path = path)
+  # Connect to remote repo if possible
+  repo_url <- worcs:::parse_repo(remote_repo = remote_repo, verbose = FALSE)
+  valid_repo <- !is.null(repo_url)
+  if(valid_repo){
+    tryCatch({
+      Args_gert <- list(
+        name = "origin",
+        url = remote_repo,
+        repo = path
+      )
+      do.call(gert::git_remote_add, Args_gert)
+      col_message(paste0("Connected to remote repository at ", remote_repo), verbose = verbose)
+    }, error = function(e){
+      col_message("Could not connect to a remote 'GitHub' repository. You are working with a local 'Git' repository only.", success = FALSE, verbose = verbose)
+    })
+  } else {
+    col_message("No valid 'GitHub' address provided. You are working with a local 'Git' repository only.", success = FALSE)
+  }
+  
+  # 3. Add theory file
   has_theory_file <- !is.null(theory_file)
   if(has_theory_file){
     existing_theory_file <- file.exists(theory_file)
-  }
-  if(existing_theory_file) theory_file <- normalizePath(theory_file)
-  
-  # 1. Create project folder
-  oldwd <- getwd()
-  if(!dir.exists(path)){
-    dir.create(path)
-    setwd(path)
-  }
-  if(has_theory_file){
+    theory_file <- normalizePath(theory_file)
     if(existing_theory_file){
       file.copy(theory_file, file.path(getwd(), basename(theory_file)))
     } else {
-      file.create(file.path(getwd(), theory_file))
+      file.create(file.path(path, theory_file))
     }
   }
+  
   # 1. Add LICENSE file
-  # Begin license
   if(!add_license == "none"){
-    tryCatch({
-      dir.create(path, recursive = TRUE, showWarnings = FALSE)
-      
-      # copy 'resources' folder to path
-      license_dir = system.file('rstudio', 'templates', 'project', 'licenses', package = 'worcs', mustWork = TRUE)
-      license_file <- file.path(license_dir, paste0(add_license, ".txt"))
-      file.copy(license_file, file.path(path, "LICENSE"), copy.mode = FALSE)
-      col_message("Writing license file.", verbose = verbose)
-    }, error = function(e){
-      col_message("Writing license file.", success = FALSE)
-    })
+    add_license_file(repo = path, license = add_license)
   }
-  # End license
+  
   # 1. Add readme.md
+  lines_readme <- c("# FAIR theory: Theory Title Goes Here", "", "# Description", 
+    "", "This is a FAIR theory, see Van Lissa et al., in preparation.", "", "# Interoperability", "", "Explain what the theory can be reused for, and how.", 
+    "", "# Contributing", 
+    "", "If you want to contribute to this project, please get involved. You can do so in three ways:", 
+    "",
+    "1. **To discuss the current implementation and discuss potential changes**, file a ‘GitHub’ issue", 
+    "2. **To directly propose changes**, send a pull request containing the proposed changes",
+    "3. **To create a derivative theory**, please fork the repository",
+    "",
+    "If you fork the repository, please cite this repository (see below), and add it as a related work (below and by adding the appropriate metadata on Zenodo).",
+    "",
+    "By participating in this project, you agree to abide by the [Contributor Covenant](https://www.contributor-covenant.org/version/2/0/code_of_conduct.html).", 
+    "", "## Related works", "", "Optionally, cite the canonical reference for the theory implemented in this repository here. This is redundant with adding the cross-reference in Zenodo, but may be useful nonetheless.", 
+    "", "## Citing this work", "", "See this project's Zenodo page for the preferred citation."
+    ) 
+  if(valid_repo){
+    lines_readme[15:17] <- paste0(lines_readme[15:17], " [here](", repo_url, c("/issues)", "/pulls)", "/fork)"))
+  }
+
+  )
   # * Include project Title, Description, Usage of the theory file, How to Contribute, How to Cite the FAIR theory, Related Works (e.g., the paper that documents the theory)
   # 1. Add .zenodo.json file to provide metadata that will allow Zenodo to index the repository as a FAIR theory
   # 1. Initialize a Git repository in the project folder
